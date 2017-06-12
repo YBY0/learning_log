5420e2a0efd91147dc0ae05bb39c50ecb1ef299e
Biyu Yan <517207812@qq.com>
2017-06-12 21:23:50 +0800

    fix the STATIC-ROOT problem

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -152,7 +152,7 @@ if os.getcwd() =='/app':
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
