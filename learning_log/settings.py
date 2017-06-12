3ca994e4694c861281af49508ce178da2d477ef2
Biyu Yan <517207812@qq.com>
2017-06-12 13:35:40 +0800

    fix the wrong setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -150,7 +150,7 @@ if os.getcwd() =='/app':
 
     #static setting
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIX_ROOT = 'staticfiles'
+    STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
